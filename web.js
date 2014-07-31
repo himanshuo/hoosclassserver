ce4af9ca3e05e0046e8046814feb3c6d86611c9c
himanshu <himanshu@himanshu.(none)>
2014-07-30 22:01:30 -0400

    testing

--- web.js
+++ web.js
@@ -100,11 +100,11 @@ app.get('/', function(req, res) {
    //-------------------------------
     var request = require('request');
     var yql = "https://query.yahooapis.com/v1/public/yql?q=\"";
-    var query = String.format("select * from html where url='http://rabi.phys.virginia.edu/mySIS/CS2/page.php?Semester=1148&Type=Group&Group={0}' and ", subject);
+    var query = encodeURIComponent(String.format("select * from html where url='http://rabi.phys.virginia.edu/mySIS/CS2/page.php?Semester=1148&Type=Group&Group={0}'", subject));
     var xpath = String.format("xpath='//tr[contains(.,\"{0}\")]'", classNum);
     var params = "&format=json";
-    var url = encodeURIComponent(yql + query + xpath + params);
-    res.send(url);
+    var url = yql + query +" and " + xpath + params;
+    //res.send(url);
     request(url, function(error, response, body){
 
         res.send(body);
