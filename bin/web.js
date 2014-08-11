7f911f9c1092dfc63fe20043ce4839f02d44e26b
himanshu <himanshu@himanshu.(none)>
2014-08-11 15:24:49 -0400

    more

--- bin/web.js
+++ bin/web.js
@@ -355,7 +355,7 @@ function dostuff() {
 
                                 sendUpdateEmail(row.email, listOfChanges);
                                 //update these values in database
-                                var updateQuery = String.format("update alerts set professor={0}, timing={1}, units={2}",course.professor, course.timing, course.units);
+                                var updateQuery = String.format("update alerts set professor='{0}', timing='{1}', units={2}",course.professor, course.timing, course.units);
                                 try{console.log("called");
                                     client.query(updateQuery, function(u_err, u_res){
                                         console.log("update failed due to " +u_err);
